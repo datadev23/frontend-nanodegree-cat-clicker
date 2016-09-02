b52ee572f69f46e69f869fe6c89fe3f34c7f4116
maindev <systemdevcloud@live.co.uk>
2016-09-02 15:52:17 +0100

    my first submit code clicker

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+var counter = 0;
+var countertwo = 0;
+alert("test");
+var firstcat = "Jingles"
+$("#catbutton").click(function() {
+counter++;
+
+	alert("button click" + counter);
+
+	
+
+});
+
+
+$("#catbutton2").click(function() {
+countertwo++;
+
+	alert("button two click" + countertwo);
+
+});
+
+});	
